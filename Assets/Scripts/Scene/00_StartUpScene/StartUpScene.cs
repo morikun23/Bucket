a7d393a8d907cb38aa4b297eff17e70c938369ee
森田 勝 <morisug4645@gmail.com>
2018-01-31 14:57:40 +0900

    【環境構築】シーン遷移のサイクルを実装

---
+++ Assets/Scripts/Scene/00_StartUpScene/StartUpScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ToyBox;
+
+namespace Bucket {
+	public class StartUpScene : Scene {
+
+		public override IEnumerator OnEnter() {
+			AppManager.Instance.fade.Fill(Color.black);
+			yield break;
+		}
+
+		public override IEnumerator OnUpdate() {
+			yield break;
+		}
+
+		public override IEnumerator OnExit() {
+			UnityEngine.SceneManagement.SceneManager.LoadScene("SceneTitle");
+			yield return null;
+
+		}
+	}
+}
---
+++ Assets/Scripts/Scene/01_TitleScene/TitleScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ToyBox;
+
+namespace Bucket {
+	public class TitleScene : Scene {
+
+		public override IEnumerator OnEnter() {
+			AppManager.Instance.fade.StartFade(new FadeIn() , Color.black , 0.5f);
+			yield return new WaitWhile(AppManager.Instance.fade.IsFading);
+		}
+
+		public override IEnumerator OnUpdate() {
+			yield break;
+		}
+
+		public override IEnumerator OnExit() {
+			AppManager.Instance.fade.StartFade(new FadeOut() , Color.black , 0.5f);
+			yield return new WaitWhile(AppManager.Instance.fade.IsFading);
+			UnityEngine.SceneManagement.SceneManager.LoadScene("SceneMain");
+		}
+	}
+}
