6373dc83ef63155d858ae175548b61f9f72fe265
naonosuke8267 <naonosuke8267@gmail.com>
2018-02-02 09:31:18 +0900

    【素材】作成した現段階の画像をインポート、ConceptArtシーンで見栄え確認中

---
+++ Assets/SandBox/Kuno/AnimTest_Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTest_Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			transform.position += new Vector3(3 * Time.deltaTime,0,0);
+		}
+	}
+}
