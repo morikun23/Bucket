d18dfca702931ed37392ca87d2d6ff5b1a760290
森田 勝 <morisug4645@gmail.com>
2018-02-05 03:02:23 +0900

    【プレイヤー実装】接地判定コンポーネントにレイヤー指定の機能を追加

--- Assets/Scripts/Actor/GroundDetector.cs
+++ Assets/Scripts/Actor/GroundDetector.cs
@@ -15,37 +15,20 @@ namespace ToyBox {
 		[SerializeField]
 		private GameObject m_noticeTarget;
 
+		[SerializeField]
+		private LayerMask m_targetLayer;
+
 		private BoxCollider2D m_collider;
 
 		private void Awake() {
 			m_collider = GetComponent<BoxCollider2D>();
 		}
 
-		//private void OnTriggerEnter2D(Collider2D arg_collider) {
-		//	if(arg_collider.gameObject.layer == LayerMask.NameToLayer("Ground")
-		//		|| arg_collider.gameObject.layer == LayerMask.NameToLayer("Landable")) {
-		//		m_noticeTarget.SendMessage("OnGroundEnter");
-		//	}
-		//}
-
-		//private void OnTriggerExit2D(Collider2D arg_collider) {
-
-		//	Debug.Log(arg_collider.gameObject.name);
-
-		//	if(!Physics2D.IsTouchingLayers(m_collider , 1 << LayerMask.NameToLayer("Ground"))) {
-		//		if (arg_collider.gameObject.layer == LayerMask.NameToLayer("Ground")
-		//			|| arg_collider.gameObject.layer == LayerMask.NameToLayer("Landable")) {
-		//			m_noticeTarget.SendMessage("OnGroundExit");
-		//		}
-		//	}
-		//}
-
-
 		private void FixedUpdate() {
 
 			RaycastHit2D hitInfo = Physics2D.BoxCast(
 				transform.position , m_collider.bounds.size ,
-				0 , Vector2.zero , 0 , 1 << LayerMask.NameToLayer("Ground") | 1 << LayerMask.NameToLayer("Landable"));
+				0 , Vector2.zero , 0 , m_targetLayer);
 
 			GroundState prevState;
 
@@ -54,8 +37,8 @@ namespace ToyBox {
 			if(m_currentGroundState != prevState) {
 				m_currentGroundState = prevState;
 				switch (m_currentGroundState) {
-					case GroundState.GROUND: m_noticeTarget.SendMessage("OnGroundEnter"); break;
-					case GroundState.AIR: m_noticeTarget.SendMessage("OnGroundExit"); break;
+					case GroundState.GROUND: m_noticeTarget.SendMessage("OnGroundEnter",hitInfo.collider); break;
+					case GroundState.AIR: m_noticeTarget.SendMessage("OnGroundExit", hitInfo.collider); break;
 				}
 			}
 			
