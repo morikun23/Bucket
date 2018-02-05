562a04976616dea1149c3d604ebf7911e3d7ce8c
naonosuke8267 <naonosuke8267@gmail.com>
2018-02-05 23:28:33 +0900

    【Scene】タイトルシーン作成

--- Assets/Scripts/Scene/01_TitleScene/TitleScene.cs
+++ Assets/Scripts/Scene/01_TitleScene/TitleScene.cs
@@ -2,23 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 using ToyBox;
+using UnityEngine.SceneManagement;
 
-namespace Bucket {
-	public class TitleScene : Scene {
+namespace Herope {
+	public class TitleScene : ToyBox.Scene {
 
-		public override IEnumerator OnEnter() {
-			AppManager.Instance.fade.StartFade(new FadeIn() , Color.black , 0.5f);
-			yield return new WaitWhile(AppManager.Instance.fade.IsFading);
+		[SerializeField]
+		Animator m_clickStart;
+
+		[SerializeField]
+		AudioSource bgm_;
+
+		public override IEnumerator OnEnter () {
+			//準備
+
+			//フェード
+			Fade fade = AppManager.Instance.fade;
+			fade.Fill(Color.black);
+			yield return new WaitForSeconds(0.5f);
+
+			fade.StartFade(new FadeIn() , Color.black , 0.5f);
+			yield return new WaitWhile(fade.IsFading);
 		}
 
-		public override IEnumerator OnUpdate() {
+		public override IEnumerator OnUpdate () {
+			yield return new WaitWhile(() => !Input.GetMouseButtonUp(0));
+
+			//AudioSource source = AppManager.Instance.m_audioManager.CreateSe ("Sys_Enter");
+			//source.Play ();
+
+			m_clickStart.Play("Flash");
+
+			for(int i = 1;i <= 60;i ++){
+				bgm_.volume = 1f - ((float)i/60f);
+				yield return null;
+			}
+
+		}
+
+		public override IEnumerator OnExit () {
+
+			//フェード
+			Fade fade = AppManager.Instance.fade;
+
+			fade.StartFade(new FadeOut() , Color.black , 0.5f);
+
+			yield return new WaitWhile(fade.IsFading);
+			//シーン遷移
+			SceneManager.LoadScene("SceneMain");
 			yield break;
 		}
 
-		public override IEnumerator OnExit() {
-			AppManager.Instance.fade.StartFade(new FadeOut() , Color.black , 0.5f);
-			yield return new WaitWhile(AppManager.Instance.fade.IsFading);
-			UnityEngine.SceneManagement.SceneManager.LoadScene("SceneMain");
-		}
 	}
 }
