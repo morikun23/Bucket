9daeeed4833f57267eb40f85687c63b3559ff09f
naonosuke8267 <naonosuke8267@gmail.com>
2018-02-05 15:19:49 +0900

    【Stage】画像追加、StageBuildシーンでステージ化

---
+++ Assets/SandBox/Kuno/Scroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour {
+
+	[SerializeField]
+	float scrollSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Horizontal")){
+			transform.Translate (new Vector3(Input.GetAxis("Horizontal") * scrollSpeed,0,0));
+		}
+	}
+}
