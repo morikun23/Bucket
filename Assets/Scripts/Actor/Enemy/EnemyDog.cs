217fed07964ecf83cf06fdedaae0899c3c383da2
[大山 亮] <oyameycry3024@gmail.com>
2018-02-06 23:13:00 +0900

    【エネミー】基底クラスを元に敵ごとのクラスを作成

---
+++ Assets/Scripts/Actor/Enemy/EnemyDog.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bucket
+{
+
+    public class EnemyDog : EnemyBehaviour
+    {
+
+        // Use this for initialization
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        // Update is called once per frame
+        protected override void Update()
+        {
+            base.Update();
+
+            if (m_isPlayerFound)
+            {
+                StartCoroutine(DoesFindPlayer());
+            }
+
+            if (m_isSwoon)
+            {
+                StartCoroutine(DoesSwoon());
+            }
+
+            if (m_isItemFound)
+            {
+                StartCoroutine(DoesFindItem());
+            }
+        }
+
+        //プレイヤーを見つけたら、Findのアニメが終わるまで待機
+        IEnumerator DoesFindPlayer()
+        {
+            m_anim.SetBool("Find", true);
+            m_anim.Update(0);
+            yield return new Tsubakit.WaitForAnimation(m_anim,0);
+            m_anim.SetBool("Find", false);
+            OnSetTargetPlayer();
+        }
+
+        //気絶したら、気絶のアニメが終わるまで待機
+        IEnumerator DoesSwoon()
+        {
+            m_anim.SetBool("Stun", true);
+            m_anim.Update(0);
+            yield return new Tsubakit.WaitForAnimation(m_anim, 0);
+
+            m_anim.SetBool("Stun", false);
+            m_isSwoon = false;
+        }
+
+        //アイテムを見つけたら１秒待機
+        IEnumerator DoesFindItem()
+        {
+
+            yield return new WaitForSeconds(1);
+            m_isItemFound = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Actor/Enemy/EnemyDrone.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bucket
+{
+
+    public class EnemyDrone : EnemyBehaviour
+    {
+
+        // Use this for initialization
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        // Update is called once per frame
+        protected override void Update()
+        {
+            base.Update();
+
+            if (m_isPlayerFound)
+            {
+                StartCoroutine(DoesFindPlayer());
+            }
+
+            if (m_isSwoon)
+            {
+                StartCoroutine(DoesSwoon());
+            }
+
+            if (m_isItemFound)
+            {
+                StartCoroutine(DoesFindItem());
+            }
+        }
+
+        //プレイヤーを見つけたら、１秒たつまで待機
+        IEnumerator DoesFindPlayer()
+        {
+
+            yield return new WaitForSeconds(1);
+            OnSetTargetPlayer();
+        }
+
+        //気絶したら、3秒たつまで待機
+        IEnumerator DoesSwoon()
+        {
+
+            yield return new WaitForSeconds(3);
+            m_isSwoon = false;
+        }
+
+        //アイテムを見つけたら１秒待機
+        IEnumerator DoesFindItem()
+        {
+
+            yield return new WaitForSeconds(1);
+            m_isItemFound = false;
+        }
+    }
+
+}
